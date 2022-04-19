d1a53df57f101f3904543d7653803050d7e8b523
ruthenian8 <ruthenian8@gmail.com>
2022-04-19 12:30:23 +0300

    rework generic response

--- dff_generic_response/__init__.py
+++ dff_generic_response/__init__.py
@@ -4,4 +4,17 @@ __author__ = "Daniil Ignatiev"
 __email__ = "ruthenian8@gmail.com"
 __version__ = "0.0.1"
 
-from .dff_generic_response import GenericResponse
+from .dff_generic_response import *
+
+__all__ = [
+    "Command",
+    "Audio",
+    "Video",
+    "Image",
+    "Attachment",
+    "Link",
+    "Button",
+    "Keyboard",
+    "Markup",
+    "GenericResponse",
+]
